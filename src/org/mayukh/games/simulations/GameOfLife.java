51546642bd7f0cf41547de0b0985019659714e7b
mayukh42 <mayukh.mukherjee@gmail.com>
2016-05-27 02:17:05 +0530

    Completed basic Game of Life

--- src/org/mayukh/games/simulations/GameOfLife.java
+++ src/org/mayukh/games/simulations/GameOfLife.java
@@ -12,20 +12,27 @@ import java.util.Random;
  */
 public class GameOfLife implements Game {
 
-    private int[] board;
-    private final int size = 16;
+    private int[] currBoard, nextBoard;
+    private final int size = 8;
     private Random generator;
 
     public GameOfLife () {
-        board = new int[size*size];
+        currBoard = new int[size*size];
+        nextBoard = new int[size*size];
         generator = new Random();
+    }
+
+    void showBoard () {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++)
+                System.out.printf("%6s", currBoard[i*size + j] == 1 ? "X" : ".");
+            System.out.println();
+        }
     }
 
     int getIndex (int row, int col) {
         row = (row + size) % size;
         col = (col + size) % size;
-
-        System.out.println(row + ", " + col);
         return row * size + col;
     }
 
@@ -42,7 +49,7 @@ public class GameOfLife implements Game {
         sw  s   se
      */
 
-    public int[] getNeighbors (int row, int col) {
+    int[] getNeighbors (int row, int col) {
         int[] neighbors = new int[8];
 
         neighbors[0] = getIndex(row-1, col-1);
@@ -59,30 +66,66 @@ public class GameOfLife implements Game {
         return neighbors;
     }
 
+    void evaluate () {
+        for (int i = 0; i < size*size; i++)
+            nextBoard[i] = 0;
+        for (int i = 0; i < size*size; i++) {
+            Point p = getCoordinates(i);
+            int liveNeighbors = Arrays.stream(getNeighbors(p.getX(), p.getY())).map(
+                    n -> currBoard[n]
+            ).reduce(
+                    0,
+                    (a, b) -> a + b
+            );
+            int alive = currBoard[i];
+
+            // Conway's rules
+            if (alive == 1 && liveNeighbors < 2)
+                alive = 0;
+            else if (alive == 1 && (liveNeighbors == 2 || liveNeighbors == 3))
+                alive = 1;
+            else if (alive == 1 && liveNeighbors > 3)
+                alive = 0;
+            else if (alive == 0 && liveNeighbors == 3)
+                alive = 1;
+
+            nextBoard[i] = alive;
+        }
+        System.arraycopy(nextBoard, 0, currBoard, 0, size*size);
+    }
+
 
     @Override
     public void build() {
+        // TODO: Use generator
         Point[] points = {
                 Point.of(0, 0),
-                Point.of(7, 7),
-                Point.of(15, 15),
-                Point.of(0, 4),
-                Point.of(15, 4),
-                Point.of(4, 0),
-                Point.of(4, 15)
+                Point.of(size/2 - 1, size/2 - 1),
+                Point.of(size-1, size-1),
+                Point.of(0, size/4),
+                Point.of(size-1, size/4),
+                Point.of(size/4, 0),
+                Point.of(size/4, size-1)
         };
         Arrays.stream(points).forEach(
-                point -> System.out.println(Arrays.toString(getNeighbors(point.getX(), point.getY())))
+                point -> currBoard[getIndex(point.getX(), point.getY())] = 1
         );
     }
 
     @Override
     public void draw() {
-
+        System.out.println("Seed state]");
+        showBoard();
     }
 
     @Override
     public void play() {
-
+        int i = 0;
+        while (i < 200) {
+            evaluate();
+            i++;
+        }
+        System.out.println("After " + i + " rounds]");
+        showBoard();
     }
 }
